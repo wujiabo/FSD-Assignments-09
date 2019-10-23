4365fd753646e79ced015871f66dc13f3abb4d90
Jiabo.Wu <wujbow@cn.ibm.com>
2019-10-23 15:22:50 +0800

    docker

--- src/main/java/com/wujiabo/fsd/controller/DBController.java
+++ src/main/java/com/wujiabo/fsd/controller/DBController.java
@@ -2,6 +2,7 @@ package com.wujiabo.fsd.controller;
 
 import com.wujiabo.fsd.service.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ public class DBController {
     private DBService dbService;
 
 
+    @CrossOrigin
     @GetMapping(value = "/dbVersion")
     public String dbVersion(){
         String version = dbService.dbVersion();
